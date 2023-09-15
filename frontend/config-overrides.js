1aa51c40a27a6576d729bff2f77af2cbc0930c5b
ryo121w <ryoutarou9121@icloud.com>
2023-09-15 23:07:54 +0900

    commit

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,12 +1,18 @@
 // config-overrides.js
 module.exports = function override(config, env) {
-    // ...その他の設定
-  
-    config.resolve.fallback = { 
-      ...config.resolve.fallback,
-      "stream": require.resolve("stream-browserify")
-    };
-  
-    return config;
+  // ...その他の設定
+
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "stream": require.resolve("stream-browserify"),
   };
-  
+
+  if (env === 'development') {
+    config.devServer = {
+      ...(config.devServer || {}), // 既存のdevServer設定を維持
+      allowedHosts: ['near-infrared-frontend'],
+    };
+  }
+
+  return config;
+};
